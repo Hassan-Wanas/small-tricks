e07b79bb0d7a0ac0d3156f55c44a4ab6e3e38ce2
حسان ونس <36311551+Hassan-Wanas@users.noreply.github.com>
2023-02-07 11:01:19 +0200

    gallery with filter

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+let section1 = document.querySelector(".one")
+let generateBtn = document.querySelector(".generate");
+let serial = document.querySelector(".serial");
+
 let nums = document.querySelectorAll(".nums .num");
 let section3 = document.querySelector(".three");
 let started = false; // Function Started ? No
@@ -9,26 +13,38 @@ let arr = ["Ahmed", "Sayed", "Ali"]
 let section4 = document.querySelector(".four");
 let random = document.querySelector(".random span");
 
+let section5 = document.querySelector(".five");
+let switchersList = document.querySelectorAll(".switcher li");
+let imgs = document.querySelectorAll(".gallery img");
+
 let up = document.querySelector(".up");
 
-window.onscroll = function () {
-  if (window.scrollY >= section3.offsetTop - 100) {
-    if (!started) {
-      nums.forEach((num) => startCount(num));
-    }
-    started = true;
+generateBtn.addEventListener("click", function () {
+  let characters = "012356789ABCDEF";
+  let charsCount = 6;
+  let randomSerial = "";
+  for (let i = 0; i < charsCount; i++) {
+    randomSerial += characters[Math.trunc(Math.random() * characters.length)]
   }
+  serial.innerHTML = randomSerial;
+  section1.style.backgroundColor = `#${randomSerial}`
+})
 
+window.onscroll = function () {
   if (window.scrollY >= section2.offsetTop - 100) {
     bars.forEach((bar) => {
       bar.style.width = bar.dataset.width
     })
   }
 
-  if (window.scrollY >= section4.offsetTop - 100) {
-    random.textContent = `${arr[Math.trunc(Math.random() * arr.length)]}`
+  if (window.scrollY >= section3.offsetTop - 100) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+      random.textContent = `${arr[Math.trunc(Math.random() * arr.length)]}`
+    }
+    started = true;
   }
-
+  
   window.scrollY >= 500 ? up.classList.add("show") : up.classList.remove("show");
 };
 
@@ -46,5 +62,21 @@ up.addEventListener("click", function () {
   window.scrollTo({
     top: 0,
     behavior: "smooth",
+  })
+})
+
+
+switchersList.forEach((tab) => {
+  tab.addEventListener("click", function () {
+    switchersList.forEach((tab) => {
+      tab.classList.remove("active");
+      this.classList.add("active");
+    });
+    imgs.forEach((img) => {
+      img.style.display = "none";
+    });
+    document.querySelectorAll(this.dataset.cat).forEach((img) => {
+      img.style.display = "block"
+    });
   })
 })
